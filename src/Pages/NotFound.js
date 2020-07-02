661e9f1ffd84bbf4ac231e6934e03ca9b90ca8af
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-07-02 17:42:57 +0530

    Update NotFound according to Mobile view

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+// import useWindowDimensions from '../Components/UseDimension';
 // import { Link } from 'react-router-dom'
 
 export default class NotFound extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { width: 0, height: 0 };
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+      }
+      
+      componentDidMount() {
+  
+        window.addEventListener('load', this.updateWindowDimensions);  
+      }
+     
+      updateWindowDimensions() {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+        console.log(this.state.width);
+      }
+
     render() {
+
+
+        let button = <Link to="/" className="box-btn"> Return To Home Page </Link>;
+
+        if(this.state.width < 450){
+            button = <a href="/" className="box-btn"> Return To Home Page  </a>;
+        }
+
+
         return (
             <>
                 {
@@ -15,7 +43,8 @@ export default class NotFound extends Component {
                                 <img src="assets/images/404.gif" alt="error" />
                                 <h3>Oops! Page Not Found</h3>
                                 <p>The page you were looking for could not be found.</p>
-                                <a href="/" className="box-btn"> Return To Home Page  </a>
+                                {button}
+                                {/* <a href="/" className="box-btn"> Return To Home Page  </a> */}
                                 {/* <Link to="/" className="box-btn">
                                 Return To Home Page   </Link> */}
                             </div>
