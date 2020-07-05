59158ea44d25cd880e929dc26b4bb48e33e9bac0
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-07-05 08:08:52 +0530

    Update css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React  from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Pages/Home';
+ import Home from './Pages/Home';
 import Loader from "./Components/Loader";
 import Header from "./Components/Header";
 import Footer from './Components/Footer';
-import Aboutus from './Pages/AboutUs';
+ import Aboutus from './Pages/AboutUs';
 import Solutions from './Pages/Solutions';
 import NotFound from './Pages/NotFound';
  
-
+ 
 class App extends React.Component {
   
   render() {
@@ -18,11 +18,10 @@ class App extends React.Component {
       <div className="App">
         {/* <ScrollToTopOnMount></ScrollToTopOnMount> */}
         <Router>
-
-          <Loader></Loader>
+        <Loader></Loader>
           <Header></Header>
-          <Switch>
-            <Route path="/" exact component={Home}></Route>
+           <Switch>
+            <Route exact path="/"  component={Home}></Route>
             <Route exact path="/about" component={Aboutus}></Route>
             <Route exact path="/solutions" component={Solutions}></Route>
             <Route component={NotFound}></Route>
@@ -32,7 +31,7 @@ class App extends React.Component {
 
           <Footer></Footer>
 
-
+          
         </Router>
       </div>
     );
--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -55,7 +55,7 @@ export default class NotFound extends Component {
                             </div>
                         </div>
                     </div>
-                </div>;
+                </div>
                 {
                     /* End 404 Error */
                 }
