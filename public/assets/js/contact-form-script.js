3b47c5f103abf5be82e0f520aa0b0fd849819778
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-08-11 18:54:52 +0530

    Add email Functionlity

--- public/assets/js/contact-form-script.js
+++ public/assets/js/contact-form-script.js
@@ -3,7 +3,21 @@
 /*==============================================================*/
 (function ($) {
     "use strict"; // Start of use strict
-    $("#contactForm").validator().on("submit", function (event) {
+    // $("#contactForm").validator().on("submit", function (event) {
+    //     debugger;
+    //     if (event.isDefaultPrevented()) {
+    //         // handle the invalid form...
+    //         formError();
+    //         submitMSG(false, "Did you fill in the form properly?");
+    //     } else {
+    //         // everything looks good!
+    //         event.preventDefault();
+    //         submitForm();
+    //     }
+    // });
+
+    $("#contactFormButton").click(function (event) {
+        debugger;
         if (event.isDefaultPrevented()) {
             // handle the invalid form...
             formError();
@@ -16,7 +30,7 @@
     });
 
 
-    function submitForm(){
+    function submitForm() {
         // Initiate Variables With Form Content
         var name = $("#name").val();
         var email = $("#email").val();
@@ -24,35 +38,58 @@
         var phone_number = $("#phone_number").val();
         var message = $("#message").val();
 
+        debugger;
+        if (name.length == 0 && email.length == 0 && msg_subject.length == 0 && message.length == 0 && phone_number.length == 0) {
+            alert('Please enter all details');
+            $('#image-loader').fadeOut();
 
-        $.ajax({
-            type: "POST",
-            url: "assets/php/form-process.php",
-            data: "name=" + name + "&email=" + email + "&msg_subject=" + msg_subject + "&phone_number=" + phone_number + "&message=" + message,
-            success : function(text){
-                if (text == "success"){
-                    formSuccess();
-                } else {
-                    formError();
-                    submitMSG(false,text);
-                }
-            }
-        });
+            return false;
+        }
+
+        var data = 'contactName=' + name + '&contactEmail=' + email +
+            '&contactSubject=' + msg_subject + '&contactMessage=' + message + '&Phone Number=' + phone_number;
+
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "deepanshu2912@gmail.com",
+            Password: "Priya@1996",
+            To: 'ahujadeepu29@gmail.com',
+            From: email,
+            Subject: msg_subject,
+            Body: data
+        }).then(
+            message => formSuccess()
+        );
+
+
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "deepanshu2912@gmail.com",
+            Password: "Priya@1996",
+            To: email,
+            From: 'deepanshu2912@gmail.com',
+            Subject: 'Thanks for the enquiry',
+            Body: 'Hi ' + name + ' , \n Thanks for your interest we will catch early.'
+        })
+
+        return false;
+
+
     }
 
-    function formSuccess(){
+    function formSuccess() {
         $("#contactForm")[0].reset();
         submitMSG(true, "Message Submitted!")
     }
 
-    function formError(){
-        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    function formError() {
+        $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
             $(this).removeClass();
         });
     }
 
-    function submitMSG(valid, msg){
-        if(valid){
+    function submitMSG(valid, msg) {
+        if (valid) {
             var msgClasses = "h4 tada animated text-success";
         } else {
             var msgClasses = "h4 text-danger";
--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 
+import * as $ from 'jquery';
+
 export default class ContactUs extends Component {
+
+
+    sendEmail = () => {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var msg_subject = $("#msg_subject").val();
+        var phone_number = $("#phone_number").val();
+        var message = $("#message").val();
+
+        fetch('https://node-deep.herokuapp.com/email/' + name + '/' + email + '/' + phone_number + '/' + msg_subject + '/' + message + '').then(response => response.text())
+            .then(response => {
+                console.log(response);
+                $("#contactForm")[0].reset();
+                var msgClasses = "h4 tada animated text-success";
+                $("#msgSubmit").removeClass().addClass(msgClasses).text("Email Send Successfully");
+            })
+            .catch(err => {
+                console.log(err);
+                var msgClasses = "h4 text-danger"; 
+                $("#msgSubmit").removeClass().addClass(msgClasses).text("Error! Please try again.");
+            });
+
+    }
+
+
     render() {
         return (
             <>
                 {/* Home Contact Area */}
                 <section className="home-contact-area home-2-contact ptb-100">
                     <div className="container">
-                
+
                         <div className="row">
                             <div className="col-lg-6 col-md-6">
                                 <div className="content">
@@ -44,7 +71,8 @@ export default class ContactUs extends Component {
                                                 </div>
                                             </div>
                                             <div className="col-lg-12 col-md-12">
-                                                <button type="submit" className="default-btn page-btn box-btn">
+                                                {/* id="contactFormButton"  */}
+                                                <button onClick={this.sendEmail} type="button" className="default-btn page-btn box-btn">
                                                     Submit </button>
                                                 <div id="msgSubmit" className="h3 text-center hidden" />
                                                 <div className="clearfix" />
