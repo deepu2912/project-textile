dd9485a1f49d1c0df5400f3f3d23a97239d01b7a
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-08-13 10:22:01 +0530

    Update changes in Slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -54,7 +54,7 @@ export default class Slider extends Component {
                 <div className="row align-items-center">
                   <div className="col-lg-12 text-center">
                     <div className="slider-content one">
-                      <h1>Embroidery Digitizing Sevices</h1>
+                      <h1>Quality Embroidery Digitizing Sevices</h1>
                       <p>Affordable services, Best turnaround time, Low pricing</p>
                       <div className="slider-btn text-center">
                         <a href="tel:+917837967982" className="box-btn">Let’s Talk!</a>
@@ -74,7 +74,7 @@ export default class Slider extends Component {
                 <div className="row align-items-center">
                   <div className="col-lg-12 text-center">
                     <div className="slider-content one">
-                      <h1>QUALITY DIGITIZING SERVICES</h1>
+                      <h1>Quality Digitizing Services</h1>
                       <p>Quality Digitizing Services is located in Delhi, NCR (National Capital Region), India. Since 2008, we are offering good digitizing services and vector art services to thousands of people. The position that we have gained today is all because of the satisfied customers of our company. If you are viewing our site, then you will feel delighted to hear that the cost of digitizing is quite affordable i.e. $1 per 1000 stitches. Whereas the tariff for vector artwork starts from $7 that is very low in the market. One of the best things is that the edits are completely free. So, what are you thinking come in touch with us today only for getting the best digitizing service.</p>
                       <div className="slider-btn text-center">
                         <a href="tel:+917837967982" className="box-btn">Let’s Talk!</a>
@@ -95,7 +95,7 @@ export default class Slider extends Component {
                 <div className="row align-items-center">
                   <div className="col-lg-12 text-center">
                     <div className="slider-content one">
-                      <h1>VECTOR SERVICES</h1>
+                      <h1>Vector Services</h1>
                       <p>Best high-quality designs !! We can easily convert the designs and vector artwork according to your choices.</p>
                       <div className="slider-btn text-center">
                         <a href="tel:+917837967982" className="box-btn">Let’s Talk!</a>
