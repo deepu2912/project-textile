60a40b27b3eedd64e19646578319b27b5f0a0e31
deepu2912 <36232653+deepu2912@users.noreply.github.com>
2020-08-19 16:28:55 +0530

    sidebar

--- src/Components/VectorServiceType.js
+++ src/Components/VectorServiceType.js
@@ -8,7 +8,7 @@ export default class VectorServiceType extends Component {
       <div className="container">
         <div className="col-12">
           <div className="services-details-text">
-            <h2>VECTOR SERVICES</h2>
+            <h3>VECTOR SERVICES</h3>
             <p>If you are new to this field and are not knowing anything about vector, then we are having something for you. <b>Do you know what vector artwork is?</b> Vector artwork is one of the most important things in the field of business and marketing. Ergo, this art encourages us to provide you with the best high-quality designs. We can easily convert the designs and vector artwork according to your choices.</p>
             <h3>Vector conversion</h3>	
             <p>Raster is the image having many rectangular elements and square elements and are known as pixels. Therefore, all the details mixed up together are referred to as raster. Raster images are having their own benefits and purposes, hence, they are much better than vectors. When we use in promotional and branding materials, then vectors have no parallel use to them.</p>
