96ab71cb483c49f8813237df6d6d1bf814a42025
Deepanshu Ahuja <deepanshu.ahuja@tatapower-ddl.com>
2020-09-19 13:57:38 +0530

    add admin

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -92,48 +92,52 @@ export default class Admin extends Component {
         return (
             <div>
                 {/* Action Panel */}
+                
                 <section className="home-contact-area home-2-contact ptb-100">
-                    <div className="container" style={{ textAlign: 'right', padding: '9px' }}>
+                    <div className="container">
                         <button className="btn btn-info" sheet="Sheet">
                             <Link to="queries" style={{ color: 'white' }} >Queries</Link>
                         </button>
                     </div>
-                    <div className="container">
-
-                        <div className="row">
-                            <div className="col-lg-12 col-md-12">
-                                <div className="content">
-                                    <form id="contactForm">
-                                        <div className="row">
-                                            <div className="col-lg-6 col-sm-6">
-                                                <div className="form-group">
-                                                    <input type="email" name="Email ID" id="EmailID" className="form-control" required data-error="Please enter your Email" placeholder="Email ID" />
-                                                    <div className="help-block with-errors" />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 col-sm-6">
-                                                <div className="form-group">
-                                                    <input type="text" name="Password" id="Password" className="form-control" required data-error="Please enter your Password" placeholder="Password" />
-                                                    <div className="help-block with-errors" />
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-12 col-md-12">
-                                                {/* id="contactFormButton"  */}
-                                                <button onClick={this.sendEmail} type="button" className="default-btn page-btn box-btn">
-                                                    Submit </button>
-                                                <div id="msgSubmit" className="h3 text-center hidden" />
-                                                <div className="clearfix" />
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+         
                 </section>
                 {/* Action Panel */}
             </div>
         )
     }
 }
+
+
+// <div className="container">
+
+// <div className="row">
+//     <div className="col-lg-12 col-md-12">
+//         <div className="content">
+//             <form id="contactForm">
+//                 <div className="row">
+//                     <div className="col-lg-6 col-sm-6">
+//                         <div className="form-group">
+//                             <input type="email" name="Email ID" id="EmailID" className="form-control" required data-error="Please enter your Email" placeholder="Email ID" />
+//                             <div className="help-block with-errors" />
+//                         </div>
+//                     </div>
+//                     <div className="col-lg-6 col-sm-6">
+//                         <div className="form-group">
+//                             <input type="text" name="Password" id="Password" className="form-control" required data-error="Please enter your Password" placeholder="Password" />
+//                             <div className="help-block with-errors" />
+//                         </div>
+//                     </div>
+//                     <div className="col-lg-12 col-md-12">
+//                         {/* id="contactFormButton"  */}
+//                         <button onClick={this.sendEmail} type="button" className="default-btn page-btn box-btn">
+//                             Submit </button>
+//                         <div id="msgSubmit" className="h3 text-center hidden" />
+//                         <div className="clearfix" />
+//                     </div>
+//                 </div>
+//             </form>
+//         </div>
+//     </div>
+
+// </div>
+// </div>
